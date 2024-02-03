553611d63e51f71ef8406c91f0c8eda8819d7744
iAmBiel <bielvm02@gmail.com>
2024-02-03 12:56:36 -0300

    final adjusts

--- src/components/panelheader/index.tsx
+++ src/components/panelheader/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import  { auth } from "../../services/firebaseConnection"
+import '../../../styles.css'
 
 export function DashboardHeader(){
 
@@ -11,19 +12,19 @@ export function DashboardHeader(){
     return(
 
         <div className="w-full items-center flex h-10 bg-red-500 rounded-lg text-white font-medium gap-4 px-4 mb-4">
-            <Link className='hover:text-red-900' to='/'>
+            <Link className={location.pathname === '/' ? 'active-link' : 'hover:text-red-900 '} to='/'>
                 Home
             </Link>
 
             <p>|</p>
 
-            <Link className='hover:text-red-900' to='/dashboard'>
+            <Link className={location.pathname === '/dashboard' ? 'active-link' : 'hover:text-red-900 '} to='/dashboard'>
                 Dashboard
             </Link>
 
             <p>|</p>
 
-            <Link className='hover:text-red-900' to='/dashboard/new'>
+            <Link className={location.pathname === '/dashboard/new' ? 'active-link' : 'hover:text-red-900 '} to='/dashboard/new'>
                 Novo Carro
             </Link>
 
